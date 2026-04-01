f5b50626674d6616e106a8458868e486f712a335
Max Wells <maxwells.pro@proton.me>
2026-04-01 09:44:43 +0700

    fmt: auto-format before deploy

--- app/src/components/mod.rs
+++ app/src/components/mod.rs
@@ -2,9 +2,9 @@ pub mod logos;
 pub mod navigation;
 
 pub mod app_footer;
-pub mod github_stars;
 pub mod app_header;
 pub mod command_search_docs;
+pub mod github_stars;
 pub mod newsletter_signup;
 pub mod shared_sidenav_demos;
 pub mod sticky_footer;
